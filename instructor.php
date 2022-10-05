6fda377158688f3da3be5b3450ef81ddab927813
amsoltis <97993404+amsoltis@users.noreply.github.com>
2022-10-05 14:09:33 -0500

    Update instructor.php

--- instructor.php
+++ instructor.php
@@ -4,6 +4,8 @@
 
 <body>
     <h1>Instructors</h1>
+  <a href="instructor-add.php" class="btn btn-primary">Add New</a>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 <table class="table table-striped">
   <thead>
     <tr>
@@ -47,10 +49,7 @@ $conn->close();
 ?>
   </tbody>
     </table>
-    <a href="instructor-add.php" class="btn btn-primary">Add New</a>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-  </body>
-
+      </body>
 <footer>
 <?php require_once("footer.php"); ?>
 </footer>
