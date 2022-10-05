8f3c853b45f82ced40952fa3b9b63cc82dc1e3b9
amsoltis <97993404+amsoltis@users.noreply.github.com>
2022-10-05 13:56:42 -0500

    Update instructor.php

--- instructor.php
+++ instructor.php
@@ -1,1 +1,55 @@
+  <head>
+<?php require_once("header.php"); ?>
+</head>
 
+<body>
+    <h1>Instructors</h1>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>InstructorID</th>
+      <th>Name</th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+$servername = "localhost";
+$username = "asoltiso_asoltis";
+$password = "OUcreate!";
+$dbname = "asoltiso_homework3";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT InstructorID, LastName, FirstName FROM Instructor";
+//echo $sql;
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+  <tr>
+    <td><?=$row["InstructorID"]?></td>
+    <td><a href="page3.php?id=<?=$row["InstructorID"]?>"><?=$row["LastName"]." "?><?=$row["FirstName"]?></a></td>
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </tbody>
+    </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+
+<footer>
+Click the 'Name' to see only that Instructors Information
+</footer>
