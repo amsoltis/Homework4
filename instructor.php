2d46e2aee192c9d43b6d55f61b2c40b21ef20ba2
amsoltis <97993404+amsoltis@users.noreply.github.com>
2022-10-05 14:10:41 -0500

    Update instructor.php

--- instructor.php
+++ instructor.php
@@ -6,7 +6,6 @@
     <h1>Instructors</h1>
   <a href="instructor-add.php" class="btn btn-primary">Add New</a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-<br />
   <table class="table table-striped">
   <thead>
     <tr>
