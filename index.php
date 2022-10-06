7125da3d164b8f01ae6c2d0034760365314e8dce
Andrew Soltis <ams.soltis@ou.edu>
2022-10-06 11:23:30 -0500

    Hello

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <head>
 <?php require_once("header.php"); ?>
 </head>
-  
-  <body>
-  <br>
-    This webpage contains 4 tables from an sql database. (Instructors, Courses, Cars, Birds)
+
+<body>
+    <br>
+        This webpage contains 4 tables from an sql database. (Instructors, Courses, Cars, Birds)
     </br>
     <br>
         Each table allows you to add, edit, and delete records.
-        </br>
+    </br>
     <br>
         In the 'Instructors' table you can add courses taught by that professor to the courses table by using the 'Add Course' button.
     </br>
-  </body>
+</body>
 
 <footer>
-<?php require_once("footer.php"); ?>
+    <?php require_once("footer.php"); ?>
 </footer>
