dccf180e87d982264c60e702e8a957569af99d0b
Andrew Soltis <ams.soltis@ou.edu>
2022-10-06 15:21:25 -0500

    edit-save

---
+++ courses-edit-save.php
@@ -0,0 +1,40 @@
+<head>
+<?php require_once("header.php"); ?>
+</head>
+
+<body>
+
+<?php
+$servername = "localhost";
+$username = "asoltiso_asoltis";
+$password = "OUcreate!";
+$dbname = "asoltiso_homework3";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$cInstructor = $_POST['cInstructor'];
+$cNumber = $_POST['cNumber'];
+$cSection = $_POST['cSection'];
+
+$sql = "update Courses set InstructorID=?, CourseNumber=?, Section=? where CourseID=?";
+//echo $sql;
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("issi", $cInstructor, $cNumber, $cSection, $_POST['id']);
+    $stmt->execute();
+?>
+    
+    <h1>Edit Instructor</h1>
+<div class="alert alert-success" role="alert">
+  Instructor edited.
+</div>
+    <a href="instructor.php" class="btn btn-primary">Go back</a>
+  </body>
+
+<footer>
+<?php require_once("footer.php"); ?>
+</footer>
+
