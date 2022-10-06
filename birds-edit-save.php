bffa21f8346b2bcc4b6c4eab0cc4261893398098
Andrew Soltis <ams.soltis@ou.edu>
2022-10-06 16:40:18 -0500

    test

--- birds-edit-save.php
+++ birds-edit-save.php
@@ -4,7 +4,28 @@
 
 <body>
 
+<?php
+$servername = "localhost";
+$username = "asoltiso_asoltis";
+$password = "OUcreate!";
+$dbname = "asoltiso_homework3";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$bName = $_POST['bName'];
+$bColor = $_POST['bColor'];
+$bAge = $_POST['bAge'];
+
+$sql = "update Birds set bName=?, bColor=?, bAge=? where Birdid=?";
+//echo $sql;
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ssii", $bName, $bColor, $bAge, $_POST['id']);
+    $stmt->execute();
+?>
     
     <h1>Edit Birds</h1>
 <div class="alert alert-success" role="alert">
--- birds-edit.php
+++ birds-edit.php
@@ -33,14 +33,14 @@ if ($result->num_rows > 0) {
     <input type="text" class="form-control" id="Name" aria-describedby="nameHelp" name="bName" value="<?=$row['Name']?>">
   </div>
   <div class="mb-3">
-    <label for="Color" class="form-label">Color</label>
+    <label for="Color" class="form-label">Bird Color</label>
     <input type="text" class="form-control" id="Color" aria-describedby="nameHelp" name="bColor" value="<?=$row['Color']?>">
   </div>
     <div class="mb-3">
-    <label for="Age" class="form-label">Age</label>
+    <label for="Age" class="form-label">Bird Age</label>
     <input type="text" class="form-control" id="Age" aria-describedby="nameHelp" name="bAge" value="<?=$row['Age']?>">
   </div>
-  <input type="hidden" name="id" value="<?=$row['CourseID']?>">
+  <input type="hidden" name="id" value="<?=$row['Birdid']?>">
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 <?php
