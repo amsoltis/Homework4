bbc0839a22ad3f043de83ce6ef4256ab050b83e5
Andrew Soltis <ams.soltis@ou.edu>
2022-10-06 15:37:00 -0500

    new course add

---
+++ courses-add-save.php
@@ -0,0 +1,40 @@
+<head>
+<?php require_once("header.php"); ?>
+</head>
+
+<body>
+    
+<?php
+$servername = "localhost";
+$username = "asoltiso_asoltis";
+$password = "OUcreate!";
+$dbname = "asoltiso_homework3";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$cInstructor = $_POST['cInstructor'];
+$cNumber = $_POST['cNumber'];
+$cSection = $_POST['cSection'];
+
+$sql = "insert into Courses (InstructorID, CourseNumber, Section) value (?, ?, ?)";
+//echo $sql;
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("iss", $cInstructor, $cNumber, $cSection);
+    $stmt->execute();        
+?>
+    
+    <h1>Add Course</h1>
+<div class="alert alert-success" role="alert">
+  New course added.
+</div>
+    <a href="course.php" class="btn btn-primary">Go back</a>
+  </body>
+
+<footer>
+<?php require_once("footer.php"); ?>
+</footer>
+
---
+++ courses-add.php
@@ -0,0 +1,26 @@
+<head>
+<?php require_once("header.php"); ?>
+</head>
+
+<body>
+<form method="post" action="courses-edit-save.php">
+<div class="mb-3">
+    <label for="courseNumber" class="form-label">Course Number</label>
+    <input type="text" class="form-control" id="courseNumber" aria-describedby="nameHelp" name="cNumber" value="<?=$row['CourseNumber']?>">
+  </div>
+  <div class="mb-3">
+    <label for="Section" class="form-label">Section</label>
+    <input type="text" class="form-control" id="Section" aria-describedby="nameHelp" name="cSection" value="<?=$row['Section']?>">
+  </div>
+    <div class="mb-3">
+    <label for="courseInsturctor" class="form-label">Instructor ID</label>
+    <input type="text" class="form-control" id="courseInsturctor" aria-describedby="nameHelp" name="cInstructor" value="<?=$row['InstructorID']?>">
+  </div>
+  <input type="hidden" name="id" value="<?=$row['CourseID']?>">
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+  </body>
+
+<footer>
+<?php require_once("footer.php"); ?>
+</footer>
