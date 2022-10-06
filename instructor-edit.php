25af86885777ac38958ac418375ce8e29a106b62
Andrew Soltis <ams.soltis@ou.edu>
2022-10-06 10:09:16 -0500

    Test

--- instructor-edit.php
+++ instructor-edit.php
@@ -34,7 +34,7 @@ if ($result->num_rows > 0) {
     <div id="nameHelp" class="form-text">Enter the instructor's name.</div>
   </div>
   <input type="hidden" name="id" value="<?=$row['InstructorID']?>">
-  <button type="submit" </button>
+  <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 <?php
   }
