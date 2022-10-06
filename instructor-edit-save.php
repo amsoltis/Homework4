287c700dfbb609913674e2b8898ea5a22c0ddb2d
Andrew Soltis <ams.soltis@ou.edu>
2022-10-06 10:03:00 -0500

    instructor

--- instructor-edit-save.php
+++ instructor-edit-save.php
@@ -25,6 +25,6 @@ $sql = "update Instructor set FirstName=? where InstructorID=?";
 <div class="alert alert-success" role="alert">
   Instructor edited.
 </div>
-    <a href="instructors.php" class="btn btn-primary">Go back</a>
+    <a href="instructor.php" class="btn btn-primary">Go back</a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
