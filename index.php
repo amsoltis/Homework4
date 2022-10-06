9dca2f2b457389042c5c429c2cf37daeec5b52e8
Andrew Soltis <ams.soltis@ou.edu>
2022-10-06 11:20:02 -0500

    Index Description

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     </br>
     <br>
         Each table allows you to add, edit, and delete records.
+        In the 'Instructors' table you can add courses taught by that professor to the courses table by using the 'Add Course' button.
     </br>
   </body>
 
