3d1bec978415f2fc8d84b53a373849c79af64998
amsoltis <97993404+amsoltis@users.noreply.github.com>
2022-10-10 14:15:03 -0500

    Update test.php

--- test.php
+++ test.php
@@ -2,108 +2,90 @@
 <?php require_once("header.php"); ?>
 </head>
 
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-body {font-family: Arial, Helvetica, sans-serif;}
-
-/* The Modal (background) */
-.modal {
-  display: none; /* Hidden by default */
-  position: fixed; /* Stay in place */
-  z-index: 1; /* Sit on top */
-  padding-top: 100px; /* Location of the box */
-  left: 0;
-  top: 0;
-  width: 100%; /* Full width */
-  height: 100%; /* Full height */
-  overflow: auto; /* Enable scroll if needed */
-  background-color: rgb(0,0,0); /* Fallback color */
-  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
-}
-
-/* Modal Content */
-.modal-content {
-  background-color: #fefefe;
-  margin: auto;
-  padding: 20px;
-  border: 1px solid #888;
-  width: 80%;
-}
-
-/* The Close Button */
-.close {
-  color: #aaaaaa;
-  float: right;
-  font-size: 28px;
-  font-weight: bold;
-}
-
-.close:hover,
-.close:focus {
-  color: #000;
-  text-decoration: none;
-  cursor: pointer;
-}
-</style>
-</head>
 <body>
+    <h1>Cars</h1>
+  <a href="cars-add.php" class="btn btn-primary">Add New Car</a>
+  <table class="table table-striped">
+  <thead>
+    <tr>
+      <th>CarID</th>
+      <th>Color</th>
+      <th>Make</th>
+      <th>Year</th>
+      <th></th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+$servername = "localhost";
+$username = "asoltiso_asoltis";
+$password = "OUcreate!";
+$dbname = "asoltiso_homework3";
 
-<h2>Modal Example</h2>
-
-<!-- Trigger/Open The Modal -->
-<button id="myBtn">Open Modal</button>
-
-<!-- The Modal -->
-<div id="myModal" class="modal">
-
-  <!-- Modal content -->
-  <div class="modal-content">
-    <span class="close">&times;</span>
-    <form method="post" action="courses-edit-save.php">
-<div class="mb-3">
-    <label for="courseNumber" class="form-label">Course Number</label>
-   
-  </div>
-  <div class="mb-3">
-    <label for="Section" class="form-label">Section</label>
-      </div>
-    <div class="mb-3">
-    <label for="courseInsturctor" class="form-label">Instructor ID</label>
-      </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-  </div>
-
-</div>
-
-<script>
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+$sql = "SELECT CarID, Color, Make, Year from Cars Order by CarID";
+//echo $sql;
+$result = $conn->query($sql);
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+  <tr>
+    <td><?=$row["CarID"]?></td>
+    <td><?=$row["Color"]?></td>
+    <td><?=$row["Make"]?></td>
+    <td><?=$row["Year"]?></td>
+    <td>  
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+          Launch demo modal
+        </button>
+
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                ...
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </td>
+    <td>
+      <form method="post" action="cars-delete.php">
+        <input type="hidden" name="id" value="<?=$row["CarID"]?>" />
+        <input type="submit" value="Delete" />
+      </form>
+    </td>
+  </tr>
+<?php
   }
+} else {
+  echo "0 results";
 }
-</script>
-
-</body>
-</html>
-
+$conn->close();
+?>
+  </tbody>
+    </table>
+      </body>
+<footer>
+<?php require_once("footer.php"); ?>
+</footer>
