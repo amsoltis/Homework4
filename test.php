c3c8640626e5fe0ade01624b44a122dee975402b
amsoltis <97993404+amsoltis@users.noreply.github.com>
2022-10-10 13:57:15 -0500

    Update test.php

--- test.php
+++ test.php
@@ -1,63 +1,73 @@
-<head>
+  <head>
 <?php require_once("header.php"); ?>
 </head>
 
-<!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-body {font-family: Arial, Helvetica, sans-serif;}
-
-/* The Modal (background) */
-.modal {
-  display: none; /* Hidden by default */
-  position: fixed; /* Stay in place */
-  z-index: 1; /* Sit on top */
-  padding-top: 100px; /* Location of the box */
-  left: 0;
-  top: 0;
-  width: 100%; /* Full width */
-  height: 100%; /* Full height */
-  overflow: auto; /* Enable scroll if needed */
-  background-color: rgb(0,0,0); /* Fallback color */
-  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
-}
-
-/* Modal Content */
-.modal-content {
-  background-color: #fefefe;
-  margin: auto;
-  padding: 20px;
-  border: 1px solid #888;
-  width: 80%;
-}
-
-/* The Close Button */
-.close {
-  color: #aaaaaa;
-  float: right;
-  font-size: 28px;
-  font-weight: bold;
-}
-
-.close:hover,
-.close:focus {
-  color: #000;
-  text-decoration: none;
-  cursor: pointer;
-}
-</style>
-</head>
 <body>
+    <h1>Birds</h1>
+  <a href="birds-add.php" class="btn btn-primary">Add New Bird</a>
+  <table class="table table-striped">
+  <thead>
+    <tr>
+      <th>Bird ID</th>
+      <th>Name</th>
+      <th>Color</th>
+      <th>Age</th>
+      <th></th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+$servername = "localhost";
+$username = "asoltiso_asoltis";
+$password = "OUcreate!";
+$dbname = "asoltiso_homework3";
 
-<h2>Modal</h2>
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 
-<!-- Trigger/Open The Modal -->
-<button id="myBtn">Open Modal</button>
+$sql = "SELECT Birdid, Name, Color, Age from Birds Order by Birdid";
+//echo $sql;
+$result = $conn->query($sql);
 
-<!-- The Modal -->
-<div id="myModal" class="modal">
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+  <tr>
+    <td><?=$row["Birdid"]?></td>
+    <td><?=$row["Name"]?></td>
+    <td><?=$row["Color"]?></td>
+    <td><?=$row["Age"]?></td>
+    <td>  
+        <form method="post" action="birds-edit.php">
+        <input type="hidden" name="id" value="<?=$row["Birdid"]?>" />
+        <input type="submit" value="Edit" />
+      </form>
+    </td>
+    <td>
+      <form method="post" action="birds-delete.php">
+        <input type="hidden" name="id" value="<?=$row["Birdid"]?>" />
+        <input type="submit" value="Delete" />
+      </form>
+    </td>
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </tbody>
+    </table>
+  
+  
+  <div id="myModal" class="modal">
 
   <!-- Modal content -->
   <div class="modal-content">
@@ -66,36 +76,11 @@ body {font-family: Arial, Helvetica, sans-serif;}
   </div>
 
 </div>
+  
+      </body>
 
-<script>
-// Get the modal
-var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-</script>
-
-</body>
 
 <footer>
 <?php require_once("footer.php"); ?>
