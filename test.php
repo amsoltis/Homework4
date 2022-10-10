7e59717a17ea3b292888efb9af6d7c5df279c0c1
amsoltis <97993404+amsoltis@users.noreply.github.com>
2022-10-10 14:03:40 -0500

    Update test.php

--- test.php
+++ test.php
@@ -62,7 +62,22 @@ body {font-family: Arial, Helvetica, sans-serif;}
   <!-- Modal content -->
   <div class="modal-content">
     <span class="close">&times;</span>
-    <p>Some text in the Modal..</p>
+    <form method="post" action="courses-edit-save.php">
+<div class="mb-3">
+    <label for="courseNumber" class="form-label">Course Number</label>
+    <input type="text" class="form-control" id="courseNumber" aria-describedby="nameHelp" name="cNumber" value="<?=$row['CourseNumber']?>">
+  </div>
+  <div class="mb-3">
+    <label for="Section" class="form-label">Section</label>
+    <input type="text" class="form-control" id="Section" aria-describedby="nameHelp" name="cSection" value="<?=$row['Section']?>">
+  </div>
+    <div class="mb-3">
+    <label for="courseInsturctor" class="form-label">Instructor ID</label>
+    <input type="text" class="form-control" id="courseInsturctor" aria-describedby="nameHelp" name="cInstructor" value="<?=$row['InstructorID']?>">
+  </div>
+  <input type="hidden" name="id" value="<?=$row['CourseID']?>">
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
   </div>
 
 </div>
