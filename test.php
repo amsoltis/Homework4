c5cc6dd80789571a4d8efe05fdaaa5d701f7db7e
amsoltis <97993404+amsoltis@users.noreply.github.com>
2022-10-10 14:11:55 -0500

    Update test.php

--- test.php
+++ test.php
@@ -2,38 +2,6 @@
 <?php require_once("header.php"); ?>
 </head>
 
-<?php
-$servername = "localhost";
-$username = "asoltiso_asoltis";
-$password = "OUcreate!";
-$dbname = "asoltiso_homework3";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "SELECT CourseID, InstructorID, CourseNumber, Section from Courses WHERE CourseID=?";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("i", $_POST['id']);
-$stmt->execute();
-$result = $stmt->get_result();
-
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-?>
-<?php
-  }
-} else {
-  echo "0 results";
-}
-$conn->close();
-?>
-
-
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
 body {font-family: Arial, Helvetica, sans-serif;}
@@ -94,18 +62,15 @@ body {font-family: Arial, Helvetica, sans-serif;}
     <form method="post" action="courses-edit-save.php">
 <div class="mb-3">
     <label for="courseNumber" class="form-label">Course Number</label>
-    <input type="text" class="form-control" id="courseNumber" aria-describedby="nameHelp" name="cNumber" value="<?=$row['CourseNumber']?>">
+   
   </div>
   <div class="mb-3">
     <label for="Section" class="form-label">Section</label>
-    <input type="text" class="form-control" id="Section" aria-describedby="nameHelp" name="cSection" value="<?=$row['Section']?>">
-  </div>
+      </div>
     <div class="mb-3">
     <label for="courseInsturctor" class="form-label">Instructor ID</label>
-    <input type="text" class="form-control" id="courseInsturctor" aria-describedby="nameHelp" name="cInstructor" value="<?=$row['InstructorID']?>">
-  </div>
-  <input type="hidden" name="id" value="<?=$row['CourseID']?>">
-  <button type="submit" class="btn btn-primary">Submit</button>
+      </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
 </form>
   </div>
 
