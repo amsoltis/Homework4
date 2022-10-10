f9b973b27ed4ca8ff5c06b1431d5e589a48c7259
amsoltis <97993404+amsoltis@users.noreply.github.com>
2022-10-10 14:20:01 -0500

    Update test.php

--- test.php
+++ test.php
@@ -96,7 +96,22 @@ body {font-family: Arial, Helvetica, sans-serif;}
   <!-- Modal content -->
   <div class="modal-content">
     <span class="close">&times;</span>
-    <p>Test</p>
+    <form method="post" action="cars-edit-save.php">
+<div class="mb-3">
+    <label for="Color" class="form-label">Car Color</label>
+    <input type="text" class="form-control" id="Color" aria-describedby="nameHelp" name="cColor" value="<?=$row['Color']?>">
+  </div>
+  <div class="mb-3">
+    <label for="Make" class="form-label">Car Make</label>
+    <input type="text" class="form-control" id="Make" aria-describedby="nameHelp" name="cMake" value="<?=$row['Make']?>">
+  </div>
+    <div class="mb-3">
+    <label for="Year" class="form-label">Car Year</label>
+    <input type="text" class="form-control" id="TYear" aria-describedby="nameHelp" name="cYear" value="<?=$row['Year']?>">
+  </div>
+  <input type="hidden" name="id" value="<?=$row['CarID']?>">
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
   </div>
 
 </div>
