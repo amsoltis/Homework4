292fc3c931df07fb996676b362bf3cb2b307eb60
Andrew Soltis <ams.soltis@ou.edu>
2022-10-12 13:17:03 -0500

    CarID

--- test.php
+++ test.php
@@ -88,7 +88,7 @@ body {font-family: Arial, Helvetica, sans-serif;}
 }
 </style>
 <!-- Trigger/Open The Modal -->
-<button id="myBtn">Edit</button>
+<button id="$("CarID")">Edit</button>
 
 <!-- The Modal -->
 <div id="myModal" class="modal">
