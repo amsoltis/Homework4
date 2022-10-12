7362a0728bdbe0cbbb36e79126dda841562268b4
Andrew Soltis <ams.soltis@ou.edu>
2022-10-12 13:08:47 -0500

    Test

--- instructor-add.php
+++ instructor-add.php
@@ -5,9 +5,9 @@
 <body>
 <h1>Add Instructor</h1>
 <form method="post" action="instructor-add-save.php">
-<<div class="mb-3">
+<div class="mb-3">
     <label for="instructorFirstName" class="form-label">First Name</label>
-    <input type="text" class="form-control" id="instructorFirstName" aria-describedby="nameHelp" name="iFirstName" value="<?=$row['FirstName']?>">
+    <input type="text" class="form-control" id="instructorFirstName" aria-describedby="nameHelp" name="iFirstName">
   </div>
   <div class="mb-3">
     <label for="instructorLastName" class="form-label">Last Name</label>
