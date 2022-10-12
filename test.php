705bb51a0fa04bdc9d5b0626b1590c7d43e7606a
Andrew Soltis <ams.soltis@ou.edu>
2022-10-12 13:43:58 -0500

    test

--- test.php
+++ test.php
@@ -1,23 +1,15 @@
+<!doctype html>
+<html lang="en">
   <head>
-<?php require_once("header.php"); ?>
-</head>
-
-<body>
-    <h1>Cars</h1>
-  <a href="cars-add.php" class="btn btn-primary">Add New Car</a>
-  <table class="table table-striped">
-  <thead>
-    <tr>
-      <th>CarID</th>
-      <th>Color</th>
-      <th>Make</th>
-      <th>Year</th>
-      <th></th>
-      <th></th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Instructors</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+      
+<?php
 $servername = "localhost";
 $username = "asoltiso_asoltis";
 $password = "OUcreate!";
@@ -29,100 +21,91 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-
-$sql = "SELECT CarID, Color, Make, Year from Cars Order by CarID";
-//echo $sql;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  switch ($_POST['saveType']) {
+    case 'Add':
+        $sqlAdd = "insert into Instructor (FirstName, LastName) value (?, ?)";
+        $stmtAdd = $conn->prepare($sql);
+        $stmtAdd->bind_param("ss", $_POST['iFirstName'], $_POST['iLastName']);
+        $stmtAdd->execute();   
+      echo '<div class="alert alert-success" role="alert">New instructor added.</div>';
+      break;
+    case 'Edit':
+      $sqlEdit = "update Instructor set FirstName=?, LastName=? where InstructorID=?";
+      $stmtEdit = $conn->prepare($sql);
+      $stmtEdit->bind_param("ssi", $_POST['iFirstName'], $_POST['iLastName'], $_POST['id']);
+      $stmtEdit->execute();
+      echo '<div class="alert alert-success" role="alert">Instructor edited.</div>';
+    case 'Delete':
+        $sqlDelete = "Delete From Instructor where InstructorID=?";
+        $stmtDelete = $conn->prepare($sql);
+        $stmtDelete->bind_param("i", $_POST['id']);
+        $stmtDelete->execute();
+      echo '<div class="alert alert-success" role="alert">Instructor deleted.</div>';
+  }
+}
+?>
+    
+      <h1>Instructors</h1>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          
+<?php
+$sql = "SELECT InstructorID, LastName, FirstName FROM Instructor Order by InstructorID";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
 ?>
-  <tr>
-    <td><?=$row["CarID"]?></td>
-    <td><?=$row["Color"]?></td>
-    <td><?=$row["Make"]?></td>
-    <td><?=$row["Year"]?></td>
-    <td>  
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-      <style>
-body {font-family: Arial, Helvetica, sans-serif;}
-
-/* The Modal (background) */
-.modal {
-  display: none; /* Hidden by default */
-  position: fixed; /* Stay in place */
-  z-index: 1; /* Sit on top */
-  padding-top: 100px; /* Location of the box */
-  left: 0;
-  top: 0;
-  width: 100%; /* Full width */
-  height: 100%; /* Full height */
-  overflow: auto; /* Enable scroll if needed */
-  background-color: rgb(0,0,0); /* Fallback color */
-  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
-}
-
-/* Modal Content */
-.modal-content {
-  background-color: #fefefe;
-  margin: auto;
-  padding: 20px;
-  border: 1px solid #888;
-  width: 80%;
-}
-
-/* The Close Button */
-.close {
-  color: #aaaaaa;
-  float: right;
-  font-size: 28px;
-  font-weight: bold;
-}
-
-.close:hover,
-.close:focus {
-  color: #000;
-  text-decoration: none;
-  cursor: pointer;
-}
-</style>
-<!-- Trigger/Open The Modal -->
-<button id="myBtn">Edit</button>
-
-<!-- The Modal -->
-<div id="myModal" class="modal">
-
-  <!-- Modal content -->
-  <div class="modal-content">
-    <span class="close">&times;</span>
-    <form method="post" action="cars-edit-save.php">
-<div class="mb-3">
-    <label for="Color" class="form-label">Car Color</label>
-    <input type="text" class="form-control" id="Color" aria-describedby="nameHelp" name="cColor" value="<?=$row['Color']?>">
-  </div>
-  <div class="mb-3">
-    <label for="Make" class="form-label">Car Make</label>
-    <input type="text" class="form-control" id="Make" aria-describedby="nameHelp" name="cMake" value="<?=$row['Make']?>">
-  </div>
-    <div class="mb-3">
-    <label for="Year" class="form-label">Car Year</label>
-    <input type="text" class="form-control" id="TYear" aria-describedby="nameHelp" name="cYear" value="<?=$row['Year']?>">
-  </div>
-  <input type="hidden" name="id" value="<?=$row['CarID']?>">
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-  </div>
-
-</div>
-    </td>
-    <td>
-      <form method="post" action="cars-delete.php">
-        <input type="hidden" name="id" value="<?=$row["CarID"]?>" />
-        <input type="submit" value="Delete" />
-      </form>
-    </td>
-  </tr>
+          
+          <tr>
+            <td><?=$row["InstructorID"]?></td>
+            td><?=$row["LastName"]." "?><?=$row["FirstName"]?></a></td>
+            <td>
+              <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#editInstructor<?=$row["InstructorID"]?>">
+                Edit
+              </button>
+              <div class="modal fade" id="editInstructor<?=$row["InstructorID"]?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editInstructor<?=$row["InstructorID"]?>Label" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="editInstructor<?=$row["InstructorID"]?>Label">Edit Instructor</h1>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                      <form method="post" action="">
+                        <div class="mb-3">
+                          <label for="editInstructor<?=$row["InstructorID"]?>Name" class="form-label">Name</label>
+                          <input type="text" class="form-control" id="editInstructor<?=$row["InstructorID"]?>Name" aria-describedby="editInstructor<?=$row["InstructorID"]?>Help" name="iName" value="<?=$row['instructor_name']?>">
+                          <div id="editInstructor<?=$row["InstructorID"]?>Help" class="form-text">Enter the instructor's name.</div>
+                        </div>
+                        <input type="hidden" name="iid" value="<?=$row['InstructorID']?>">
+                        <input type="hidden" name="saveType" value="Edit">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td>
+              <form method="post" action="">
+                <input type="hidden" name="iid" value="<?=$row["instructor_id"]?>" />
+                <input type="hidden" name="saveType" value="Delete">
+                <button type="submit" class="btn" onclick="return confirm('Are you sure?')">Delete</button>
+              </form>
+            </td>
+          </tr>
+          
 <?php
   }
 } else {
@@ -130,38 +113,38 @@ body {font-family: Arial, Helvetica, sans-serif;}
 }
 $conn->close();
 ?>
-  </tbody>
-    </table>
-      </body>
+          
+        </tbody>
+      </table>
+      <br />
+      <!-- Button trigger modal -->
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addInstructor">
+        Add New
+      </button>
 
-<script>
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-</script>
-
-<footer>
-<?php require_once("footer.php"); ?>
-</footer>
+      <!-- Modal -->
+      <div class="modal fade" id="addInstructor" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="addInstructorLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="addInstructorLabel">Add Instructor</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form method="post" action="">
+                <div class="mb-3">
+                  <label for="instructorName" class="form-label">Name</label>
+                  <input type="text" class="form-control" id="instructorName" aria-describedby="nameHelp" name="iName">
+                  <div id="nameHelp" class="form-text">Enter the instructor's name.</div>
+                </div>
+                <input type="hidden" name="saveType" value="Add">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
