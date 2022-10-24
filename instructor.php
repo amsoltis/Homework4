8c9bae1ab45bf483834d4e8c845ae9329e0af46c
Andrew Soltis <ams.soltis@ou.edu>
2022-10-24 14:12:14 -0500

    Done

--- instructor.php
+++ instructor.php
@@ -78,6 +78,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   
+    <select>
+<?php foreach( $results as $row ){
+    echo "<option value='" . $row[FirstName] . "'>" . $row[LastName] . "</option>";
+   }
+ ?> 
+</select>
           
         <thead>
           <tr>
