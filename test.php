c60e5f98a3a7fac7292a72d38f7699eee7aec843
Andrew Soltis <ams.soltis@ou.edu>
2022-10-24 14:00:18 -0500

    Test

--- test.php
+++ test.php
@@ -48,10 +48,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       <label for="cars">Choose a car:</label>
 
 <select name="cars" id="cars">
-  <option value="volvo">Volvo</option>
-  <option value="saab">Saab</option>
-  <option value="mercedes">Mercedes</option>
-  <option value="audi">Audi</option>
+  <option value="volvo"><?=$row["InstructorID"]?></option>
 </select>
 
       <table class="table table-striped">
